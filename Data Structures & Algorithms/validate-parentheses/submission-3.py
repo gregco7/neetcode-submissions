dc1493c53104cba28580b861d257188fe32e99c8
Gregory Courtney <courtney.g@northeastern.edu>
2026-08-23 19:38:41 -0400

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,45 @@
+#Solve valid parantheses problem by adding left brackets to a stack, and verifying their removal fulfills LIFO principles.
+
+class Solution:
+
+    #Return 0 for left Bracket, 1 for right bracket. Returns None if invalid character
+    def matchingBracket(self, char_left:str,char_right:str) -> Optional[int]:
+        intendedRight = ""
+
+        match char_left:
+            case "(":
+                intendedRight = ")"
+            case "[":
+                intendedRight = "]"
+            case "{":
+                intendedRight = "}"
+        
+        return char_right == intendedRight
+
+    def isValid(self, s: str) -> bool:
+        stack = [] #[], [
+        invalidated = 0
+
+        for char in s:
+
+            if char in ")]}":
+                #We are touching the next right char, and the this bracket char should equal the popped char from stack.
+                #If no left brackets in stack to fulfill right bracket, return false.
+
+                if len(stack) == 0:
+                    return False
+
+                popped_Character = stack.pop(-1)
+                if not self.matchingBracket(popped_Character,char):
+                    return False
+                else:
+                    invalidated -= 1
+            else:
+                stack.append(char)
+                invalidated += 1
+
+        return invalidated == 0
+        # [], inv = 0, stack = []. True?
+        # {[(]}, inv = 3, stack = [ {,[,( ]. False?
+
+        
