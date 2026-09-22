798010bde7cad28823b2c3a425f2fb6bdf527efb
Gregory Courtney <courtney.g@northeastern.edu>
2026-09-22 00:21:00 -0400

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        map1, map2 = {}, {}
+
+        for char1 in s:
+
+            if char1 in map1:
+                map1[char1] += 1
+            else:
+                map1[char1] = 1
+        
+        for char2 in t:
+            if char2 in map2:
+                map2[char2] += 1
+            else:
+                map2[char2] = 1
+    
+            
+        return map1 == map2
+        
