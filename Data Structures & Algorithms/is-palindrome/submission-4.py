b8a65b78a799f7fe538f8ab0f8b73d8afdb69078
Gregory Courtney <courtney.g@northeastern.edu>
2026-09-23 00:27:31 -0400

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,30 @@
+class Solution:
+
+    #build isalnum() myself
+    def isalnum(self,c):
+        return ((ord("a") <= ord(c) <= ord("z") or (ord("A") <= ord(c) <= ord("Z")) or (ord("0") <= ord(c) <= ord("9"))))
+
+    def isPalindrome(self, s: str) -> bool:
+
+        l,r = 0,len(s)-1
+
+        while l < r:
+            while l < r and not self.isalnum(s[l]):
+                l += 1
+            while r > l and not self.isalnum(s[r]):
+                r -= 1
+            
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l += 1
+            r -= 1
+        
+        return True
+
+
+        
+
+            
+
+        
