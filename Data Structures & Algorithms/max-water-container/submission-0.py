9567d9274c58d1ee44b805726a696e0c3cb17c9e
Gregory Courtney <courtney.g@northeastern.edu>
2026-09-24 16:26:42 -0400

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    def vol(self, heights: List[int],i:int,j:int):
+        return min(heights[i],heights[j]) * (j-i)
+
+    def maxArea(self, heights: List[int]) -> int:
+
+        l,r = 0, len(heights) - 1
+        maxVol = self.vol(heights,l,r)
+
+        #me move the pointer that has the smller height
+        while l < r:
+            
+            if heights[l] < heights[r]:
+                l += 1
+            else:
+                r -= 1 #decrement right pointer when it pointer to something smaller, or its equal. It doesn't matter which pointer you alter when they point to equal values, as two large heights are needed to outpace the bottleneck.
+            
+            maxVol = max(maxVol,self.vol(heights,l,r))
+
+        return maxVol
+
+
+        
